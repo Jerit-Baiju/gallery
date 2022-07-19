aa8717400ac950f8b52f1e6fe4d862d1fbe44b94
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-19 15:41:29 +0530

    updated favicon

--- main/urls.py
+++ main/urls.py
@@ -21,5 +21,5 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('base.urls')),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')))
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.png')))
 ]
